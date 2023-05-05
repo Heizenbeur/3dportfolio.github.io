851c56b13eee9a63d8aa4ce414c8666039381913
Heizenbeur <132521908+Heizenbeur@users.noreply.github.com>
2023-05-05 14:23:22 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Yacine Tensaouti</span>
+            Hi, I'm <span className='text-[#915EFF]'>Yacine <br className='sm:block hidden' /> Tensaouti</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I am a Researcher in Neurosciences, <br className='sm:block hidden' />
