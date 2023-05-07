5ce184d0fe788b80d891aaef401e635679dc32b5
Heizenbeur <132521908+Heizenbeur@users.noreply.github.com>
2023-05-07 08:11:35 +0200

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -116,16 +116,16 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
+      title: "PhD candidate in Neurosciences",
+      company_name: "DECAD, INCIA, CNRS, University of Bordeaux (France)",
       icon: starbucks,
       iconBg: "#383E56",
-      date: "March 2020 - April 2021",
+      date: "January 2021 - present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Title: Contribution of insular pathways in value- versus cue-guided choice behavior.",
+        "Supervision: Dr Shauna L. Parkes.",
+        "Team: Decision and Adaptation, INCIA, UMR5287.",
+        "Team leader: Dr Mathieu Wolff.",
       ],
     },
     {
