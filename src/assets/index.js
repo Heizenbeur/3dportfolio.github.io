a935093eca23ab758fbd2d05051119fa16d435f0
Heizenbeur <132521908+Heizenbeur@users.noreply.github.com>
2023-05-08 01:14:16 +0200

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import loupe from "./loupe.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -106,4 +107,5 @@ export {
   combio,
   eneuro,
   plosone,
+  loupe,
 };
