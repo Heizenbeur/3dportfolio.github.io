7d9f6df8f4a9e64f1530b26dbe052127fe8ef470
Heizenbeur <132521908+Heizenbeur@users.noreply.github.com>
2023-05-07 10:26:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
         </div>
         <p className="text-xs">
           <em>
-            This website was developed by <strong style={{color: 'white'}}>Dr. Yacine Tensaouti</strong> (React, Node.js & Three.js frameworks), <strong style={{color: 'purple'}}><a href="https://github.com/Heizenbeur/3dportfolio.github.io" target="_blank" rel="noopener noreferrer">download</a></strong> code for free.
+            Developed by <strong style={{color: 'white'}}>Dr. Yacine Tensaouti</strong> (React, Node.js & Three.js frameworks), <strong style={{color: 'purple'}}><a href="https://github.com/Heizenbeur/3dportfolio.github.io" target="_blank" rel="noopener noreferrer">download</a></strong> code for free.
           </em>
         </p>
       </div>
